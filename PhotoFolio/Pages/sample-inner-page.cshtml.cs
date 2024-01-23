bfbbff01178df19f40068a9762b745c9da8a5e66
Amir-Subaih <86726698+Amir-Subaih@users.noreply.github.com>
2024-01-23 23:10:52 +0200

    Add project files.

---
+++ PhotoFolio/Pages/sample-inner-page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PhotoFolio.Pages
+{
+    public class sample_inner_pageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
